f0937aa7e9d63b237eef51f8be380c19706cee7f
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-22 14:07:27 +0300

    Add files via upload

---
+++ Homework2.cpp
@@ -0,0 +1,379 @@
+﻿// Win32Project2.cpp: определяет точку входа для приложения.
+//
+
+#include "stdafx.h"
+#include "Win32Project2.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;								// текущий экземпляр
+TCHAR szTitle[MAX_LOADSTRING];					// Текст строки заголовка
+TCHAR szWindowClass[MAX_LOADSTRING];			// имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	MyDialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: разместите код здесь.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Инициализация глобальных строк
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_WIN32PROJECT2, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Выполнить инициализацию приложения:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32PROJECT2));
+
+	// Цикл основного сообщения:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  НАЗНАЧЕНИЕ: регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32PROJECT2));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_WIN32PROJECT2);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   НАЗНАЧЕНИЕ: сохраняет обработку экземпляра и создает главное окно.
+//
+//   КОММЕНТАРИИ:
+//
+//        В данной функции дескриптор экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится на экран главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  НАЗНАЧЕНИЕ:  обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND	- обработка меню приложения
+//  WM_PAINT	-Закрасить главное окно
+//  WM_DESTROY	 - ввести сообщение о выходе и вернуться.
+//
+//
+unsigned ShiftRigth(unsigned a, int n, int L)
+{
+
+	unsigned maska = 0;
+	for (int i = 0; i<L; i++)
+	{
+		maska <<= 1;
+		maska += 1;
+	}
+	unsigned b = a & maska;
+	b <<= (n - L);
+	a >>= L;
+	return a | b;
+}
+
+unsigned ShiftLeft(unsigned a, int n, int L)
+{
+	unsigned maska = 0;
+	for (int i = 0; i<L; i++)
+	{
+		maska <<= 1;
+		maska += 1;
+	}
+	maska <<= (n - L);
+
+	unsigned b = a & maska;
+	b >>= (n - L);
+	a <<= L;
+	return a | b;
+}
+
+
+void ShifrDeshifr(unsigned char *in, unsigned char *out, int len, int key, bool flag)
+{
+
+	int n = len / 4;
+	int m = len % 4;
+	if (m) n++;
+	unsigned char *s = in, *s2 = out;
+	unsigned a, b, gamma;
+	srand(key);
+	int k;
+	for (int i = 0; i < n; i++, s += 4, s2 += 4)
+	{
+		if (i == n - 1 && m) k = m * 8;
+		else k = 32;
+		a = 0;
+		memcpy(&a, s, k / 8);
+		if (!flag) a = ShiftRigth(a, k, 3);
+		gamma = rand();
+		gamma <<= 16;
+		gamma += rand();
+		switch (k)
+		{
+		case 24: gamma &= 0xffffff; break;
+		case 16: gamma &= 0xffff; break;
+		case 8:  gamma &= 0xff;
+		}
+		b = a ^ gamma;
+		if (flag) b = ShiftLeft(b, k, 3);
+		memcpy(s2, &b, k / 8);
+
+	}
+}
+
+char Str[512];
+char StrOut[256];
+int len = 0;
+int key;
+
+OPENFILENAME ofn;
+TCHAR NameFile[256] = "MyFile.shf";
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_CHAR:
+
+		Str[len] = wParam;
+		len++;
+		Str[len] = 0;
+
+		hdc = GetDC(hWnd);
+		SetTextColor(hdc, RGB(0, 0, 0));
+		TextOut(hdc, 40, 40, Str, len);
+		ReleaseDC(hWnd, hdc);
+		break;
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case ID_32771:
+			ZeroMemory(&ofn, sizeof(ofn));
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hWnd;
+			ofn.lpstrFile = NameFile;
+			ofn.lpstrFile[0] = '\0';
+			ofn.nMaxFile = 255;
+			ofn.lpstrFilter = "Encrypted Files (*.shf)\0*.shf\0All Files\0*.*\0";
+			ofn.nFilterIndex = 0;
+			ofn.lpstrFileTitle = NULL;
+			ofn.nMaxFileTitle = 0;
+			ofn.lpstrInitialDir = NULL;
+			ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+			ofn.lpstrDefExt = "shf";
+
+			if (GetOpenFileName(&ofn))
+			{
+
+				if (DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, MyDialog) == IDOK)
+				{
+					FILE *pF1;
+					char Shifr[512];
+					pF1 = fopen(NameFile, "rb");
+					if (pF1 == 0)
+					{
+						MessageBox(hWnd, "Error opening the file. Perhaps the file does not exist", "Error message", MB_OK | MB_ICONHAND);
+						return TRUE;
+					}
+					fread(&len, sizeof(int), 1, pF1);
+					fread(Shifr, sizeof(char), len, pF1);
+					fclose(pF1);
+					ShifrDeshifr((unsigned char *)Shifr, (unsigned char *)StrOut, len, key, false);
+					Str[len] = 0;
+					InvalidateRect(hWnd, 0, TRUE);
+				}
+				else MessageBox(hWnd, "Data is not read. The key is not inserted!!", "Message", MB_OK | MB_ICONHAND);
+			}
+
+
+			break;
+		case ID_32772:
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = hWnd;
+			ofn.lpstrFilter = "Encrypted Files (*.shf)\0*.shf\0All Files\0*.*\0";
+			ofn.lpstrFile = NameFile;
+			ofn.nMaxFile = 255;
+			ofn.lpstrFileTitle = NULL;
+			ofn.nMaxFileTitle = 0;
+			ofn.lpstrInitialDir = NULL;
+			ofn.Flags = OFN_SHOWHELP | OFN_OVERWRITEPROMPT;
+			ofn.lpstrTitle = NULL;
+			ofn.lpstrDefExt = "shf";
+			if (GetSaveFileName(&ofn))
+			{
+
+				if (DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, MyDialog) == IDOK)
+				{
+					char Shifr[512];
+					ShifrDeshifr((unsigned char *)Str, (unsigned char *)Shifr, len, key, true);
+
+
+					FILE *pF1;
+					pF1 = fopen(NameFile, "wb");
+					if (pF1 == 0)
+					{
+						MessageBox(hWnd, "Error opening the file.", "Error message", MB_OK | MB_ICONHAND);
+						return TRUE;
+					}
+					fwrite(&len, sizeof(int), 1, pF1);
+					fwrite(Shifr, sizeof(char), len, pF1);
+					fclose(pF1);
+					MessageBox(hWnd, "The data was successfully stored.", "Message", MB_OK | MB_ICONINFORMATION);
+				}
+				else MessageBox(hWnd, "Data is not saved. The key is not inserted!!", "Message", MB_OK | MB_ICONHAND);
+			}
+
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		SetTextColor(hdc, RGB(0, 0, 255));
+		TextOut(hdc, 40, 40, Str, len);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK MyDialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			if (LOWORD(wParam) == IDOK)
+			{
+				char Str[32];
+				SendDlgItemMessage(hDlg, IDC_EDIT1, WM_GETTEXT, 31, (LPARAM)Str);
+				if (sscanf(Str, "%d", &key) <1)
+				{
+					MessageBox(hDlg, "Invalid format of key. The key must be an integer", "The message about an invalid key format", MB_OK | MB_ICONHAND);
+					return (INT_PTR)TRUE;
+
+				}
+
+			}
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
